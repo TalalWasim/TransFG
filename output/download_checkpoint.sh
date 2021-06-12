32369bd10a4bd8ba2279a640e150dc27f4ad627c
TalalWasim <wasimtalalgmail.com>
2021-06-12 18:31:55 +0500

    added model download files

---
+++ output/download_checkpoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Downloading TransFG ViT_B16 backend model..."
+
+gdown --id 1NTbyt7fGpfN_AXWC8v8Gle6iqjE-6RCu
+
+echo "Downloading TransFG ViT_B32 backend model..."
+
+gdown --id 169WN42hF4L2qz7vRbbxY5v3eFTwDRaVs
+
+echo "Done..."
---
+++ pretrained_ViT/download_ViT.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Downloading ViT_B16 pretrained model..."
+
+gdown --id 1enUhWy_l02XkVhQ503ul9_8mkU1dGNvj
+
+echo "Downloading ViT_B32 pretrained model..."
+
+gdown --id 1NLbIGhuhqDB48W0syTCdYn8Xh3IIFPrV
+
+echo "Done..."
